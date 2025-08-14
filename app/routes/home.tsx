cb6d12c7f6b64b965c3fd49e0fc9b7e1687ecbbf
SadhotrA <vishalsadhotra00@gmail.com>
2025-08-14 12:31:38 +0530

    Add Navbar, ResumeCard, and ScoreCircle components; update Home route to utilize new components and display resumes; remove unused Welcome component and logo assets; add resume data and types for better structure and feedback handling.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,33 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/Navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ai Resume" },
+    { name: "description", content: "smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div>
+        <h1> Track Your Application & Resume Ratings</h1>
+        <h2>Review Your Submissions and Check AI-Powered Feedback</h2>
+      </div>
+    </section>
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map(resume => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
+
+
+  </main>
 }
