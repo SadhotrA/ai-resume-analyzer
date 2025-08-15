54f9aa6d4fd2f56f23befadad17aac56c5cfa48f
SadhotrA <vishalsadhotra00@gmail.com>
2025-08-16 04:28:20 +0530

    Enhance FileUploader component by adding a file removal button and improving the upload prompt layout. Update Upload route to handle form submission and log form data for further processing.

--- app/components/FileUpoader.tsx
+++ app/components/FileUpoader.tsx
@@ -31,13 +31,10 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
       <div {...getRootProps()}>
         <input {...getInputProps()} />
         <div className='space-y-4 cursor-pointer'>
-          <div className='mx-auto w-16 h-16 flex items-center justify-center'>
-            <img src="/icons/info.svg" alt="upload" className='size-20' />
-          </div>
           {file ? (
-            <div className='uploader-selected-file' onClick={(e)=> e.stopPropagation()}>
+            <div className='uploader-selected-file' onClick={(e) => e.stopPropagation()}>
+              <img src="/images/pdf.png" alt="pdf" className='size-10' />
               <div className='flex items-center space-x-3'>
-                <img src="/images/pdf.png" alt="pdf" className='size-10' />
                 <div>
                   <p className='text-sm font-medium to-gray-700 truncate max-w-xs'>
                     {file.name}
@@ -47,9 +44,17 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                   </p>
                 </div>
               </div>
+              <button className='p-2 cursor-pointer' onClick={(e) => {
+                onFileSelect?.(null)
+              }}>
+                <img src="/icons/cross.svg" alt="remove" className='w-4 h-4' />
+              </button>
             </div>
           ) : (
             <div>
+              <div className='mx-auto w-16 h-16 flex items-center justify-center mb-2'>
+                <img src="/icons/info.svg" alt="upload" className='size-20' />
+              </div>
               <p className='text-lg text-grey-500'>
                 <span className='font-semibold'>
                   Click to Uplaod
